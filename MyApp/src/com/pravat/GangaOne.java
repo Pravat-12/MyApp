f0f6ebeb13b043b7227efb8742dac0d8768e4ff9
RanganaGangadhar <35066304+RanganaGangadhar@users.noreply.github.com>
2018-01-16 18:17:45 +0530

    Gangadhar Commit two files

---
+++ MyApp/src/com/pravat/GangaOne.java
@@ -0,0 +1,7 @@
+package com.pravat;
+
+public class GangaOne {
+public static void main(String[] args) {
+	System.out.println("GangaOne");
+}
+}
---
+++ MyApp/src/com/pravat/GangaTWO.java
@@ -0,0 +1,7 @@
+package com.pravat;
+
+public class GangaTWO {
+public static void main(String[] args) {
+	System.out.println("GangaTWO");
+}
+}
